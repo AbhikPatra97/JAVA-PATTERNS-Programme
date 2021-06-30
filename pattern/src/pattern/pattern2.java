1883efe6b6114d1f63e66005cf209157389a7eea
AbhikPatra97 <abhik.patra2018@gmail.com>
2021-06-30 21:55:13 +0530

    JAVA PATTERNS Programme

---
+++ pattern/src/pattern/pattern2.java
@@ -0,0 +1,24 @@
+package pattern;
+
+public class pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=0;j<=i-1;j++)
+			{
+				System.out.print("");
+			}
+			
+			for(int k=4;k>=i;k--)
+			{
+				System.out.print("*");
+			}
+				System.out.println("");
+				
+		}
+
+	}
+
+}
---
+++ pattern/src/pattern/pattern3.java
@@ -0,0 +1,18 @@
+package pattern;
+
+public class pattern3
+{  
+	public static void main(String[] args) 
+    {  
+        for (int i=0; i<5; i++) //outer loop for number of rows(n) { for (int j=n-i; j>1; j--) //inner loop for spaces
+            { 
+                System.out.print(""); //print space
+            }  
+            for (int j=0; j<=5; j++ ) //inner loop for number of columns
+            { 
+                System.out.println("*"); //print star
+            } 
+  
+            System.out.println(""); //ending line after each row
+        } 
+}
